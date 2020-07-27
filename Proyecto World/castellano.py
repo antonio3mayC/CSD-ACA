3a64e6e317f24baf71205e107efa6e92267392bb
antonio.castillejo <antonio.castillejo@soltel.es>
2020-07-27 20:50:44 +0200

    Añadiendo saludos en castellano

--- Proyecto World/castellano.py
+++ Proyecto World/castellano.py
@@ -1,5 +1,10 @@
 #castellano.py
 def saludar():
 	print "Hola mundo!"
+	print "¡Hola todo el mundo!"
+	print "¡Hola Doctor Nick!"
+	print "¿Que tal?" 
+	print "¡Hombre! 
+	print "¿Donde te metes?"
 	return
 saludar()
